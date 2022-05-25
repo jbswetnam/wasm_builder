f747d77f7f49b824d39984d851546df159fd6414
Joseph Swetnam <jbswetnam@gmail.com>
2022-05-24 23:55:31 -0400

    Update wasm_builder.test.js

--- versions/working/tests/wasm_builder.test.js
+++ versions/working/tests/wasm_builder.test.js
@@ -3,14 +3,29 @@
 // you will also need the utils file in NodeJS:
 // const { test_mod, assert } = require("./testing_utils.js")
 
-const mod1 = WASMBuilder.module();
+let mod = WASMBuilder.module();
 
-test_mod(mod1, function (mod) {
+test_mod(mod, function (mod) {
   assert(mod, "empty module compiles");
 });
 
 mod1.func();
 
-test_mod(mod1, function (mod) {
+test_mod(mod, function (mod) {
   assert(mod, "empty function compiles");
+});
+
+mod = WASMBuilder.module();
+
+mod.func()
+    .param("a", "i32")
+    .param("b", "i32")
+    .result("i32")
+    .export("addTwo")
+    .body().local$get("a")
+           .local$get("b")
+           .i32$add();
+
+test_mod(mod, function (mod) {
+  assert(mod.addTwo(2, 3) === 5, "simple adding function");
 });

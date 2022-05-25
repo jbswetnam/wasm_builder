6e2805329745dfbe3e8ff987d905d63480910775
Joseph Swetnam <jbswetnam@gmail.com>
2022-05-24 23:32:58 -0400

    Update testing_utils.js

--- versions/working/tests/testing_utils.js
+++ versions/working/tests/testing_utils.js
@@ -8,9 +8,13 @@
   
   exports.test_mod = function (module, msg, cb, imp) {
     module.instantiate(imp || {}).then(function (mod) {
-      cb(mod);
-      console.log(`${msg}: ${failed.length} of ${total} tests failed`);
-      for (const f of failed) console.log(`Failed: ${f}`);
+      try {
+        cb(mod);
+        console.log(`${msg}: ${failed.length} of ${total} tests failed`);
+        for (const f of failed) console.log(`Failed: ${f}`);
+      } catch (e) {
+        console.log(`${msg} failed: ${e}`);
+      }
     });
   };
 })(

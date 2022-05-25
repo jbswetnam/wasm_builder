03d896e96c5223d0116eaac8939db8ea29ee5db0
Joseph Swetnam <jbswetnam@gmail.com>
2022-05-25 00:08:34 -0400

    Update wasm_builder.test.js

--- versions/working/tests/wasm_builder.test.js
+++ versions/working/tests/wasm_builder.test.js
@@ -18,14 +18,30 @@ test_mod(mod, function (mod) {
 mod = WASMBuilder.module();
 
 mod.func()
-    .param("a", "i32")
-    .param("b", "i32")
-    .result("i32")
-    .export("addTwo")
-    .body().local$get("a")
-           .local$get("b")
-           .i32$add();
+   .param("a", "i32")
+   .param("b", "i32")
+   .result("i32")
+   .export("add_two")
+   .body().local$get("a")
+          .local$get("b")
+          .i32$add();
 
 test_mod(mod, function (mod) {
-  assert(mod.addTwo(2, 3), 5, "simple adding function");
+  assert(mod.add_two(2, 3), 5, "simple adding function");
+});
+
+mod.func()
+   .name("return_const")
+   .result("i32")
+   .body().i32$const(4);
+
+mod.func()
+   .export("call_functions")
+   .result("i32")
+   .body().call("return_const")
+          .i32$const(5)
+          .call(0);
+
+test_mod(mod, function (mod) {
+  assert(mod.call_functions(), 9, "call functions by name and index");
 });

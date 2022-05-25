d1673ec5198d76a45666ea357a0ccf9552148ecb
Joseph Swetnam <jbswetnam@gmail.com>
2022-05-24 23:36:25 -0400

    Update wasm_builder.test.js

--- versions/working/tests/wasm_builder.test.js
+++ versions/working/tests/wasm_builder.test.js
@@ -1,16 +1,18 @@
 // uncomment and modify path as needed in NodeJS:
 // const { WASMBuilder } = require("../wasm_builder.js");
 // you will also need the utils file in NodeJS:
-// const { test_mod, assert } = require("./testing_utils.js")
+// const { test_mod, assert, test_report } = require("./testing_utils.js")
 
 const mod1 = WASMBuilder.module();
 
-test_mod(mod1, "empty module", function (mod) {
+test_mod(mod1, function (mod) {
   assert(true, "empty module compiles");
 });
 
 mod1.func();
 
-test_mod(mod1, "empty function", function (mod) {
+test_mod(mod1, function (mod) {
   assert(true, "empty function compiles");
 });
+
+test_report();
